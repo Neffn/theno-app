71138de1316670b040173d81a4acd77c6e1a4b70
Neffn <ericjfountain49@gmail.com>
2026-01-03 18:53:31 -0500

    init

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,33 @@
+import { Image, StyleSheet } from "react-native";
+
+import { Text, View } from "@/components/Themed";
+
+export default function TabTwoScreen() {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.image}
+        source={require("../../assets/images/black man.jpg")}
+      />
+      <Text style={styles.title}>Placeholder</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  image: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    marginBottom: 20,
+  },
+});
